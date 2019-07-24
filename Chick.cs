c110865028935bca7416eb3667b84d87f5c01334
angelina-perez <perezangelina0701@gmail.com>
2019-07-24 14:23:41 -0700

    Updates Chick Script
    Glitching egg slide in void Follow

--- Chick.cs
+++ Chick.cs
@@ -1,70 +1,92 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 public class Chick : MonoBehaviour
 {
 	//Needs to be shown in Inspector
-	[SerializeField] private float speed;
-	[SerializeField] private float stoppingDistance;
-	[SerializeField] private Transform hen;
+    [SerializeField] private float speed;
+    [SerializeField] private float stoppingDistance;
+    [SerializeField] private float letWalk;
+    [SerializeField] private Transform targetObj;
 
-	//Does Not Need to be shown
-	private bool hide;
-    public bool following;
+    //Does Not Need to be shown
+    private bool hide;
+    private bool following;
 	private bool isMoving;
+    private bool content;
 	SpriteRenderer mySpriteRenderer;
+	Animator anim;
+    public float stopDistract = 0f;
+    public float timerToChange = 3f;
 
 	void Awake()
 	{
 		mySpriteRenderer = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 
 		NullChecking();
-        //following = GameManager.instance.followtheHen;
     }
 
 	void Update()
 	{
 		Follow();
 		Flip();
-		HideChick();	
-	}
+		HideChick();
+		SetAnimBools();
+       
+    }
 
-	void NullChecking()
+    private void OnParticleCollision()
+    {
+        print("I'm Scared");
+    }
+
+    void NullChecking()
 	{
 		//Check to make sure values have been set in Inspector
 		Assert.AreNotEqual(0, speed);
 		Assert.AreNotEqual(0, stoppingDistance);
-		Assert.IsNotNull(hen);
+		Assert.IsNotNull(targetObj);
 	}
+
+    public void SetTarget(Transform newTarget)
+    { 
+        targetObj = newTarget;
+        timerToChange -= Time.deltaTime;
+    }
 
     void Follow()
     {
-        var henPos = new Vector2(hen.position.x, transform.position.y);
+        var henPos = new Vector2(targetObj.position.x, transform.position.y);
         following = GameManager.instance.followtheHen;
 
-        if (Vector2.Distance(transform.position, hen.position) > stoppingDistance && following)
-        { 
-        transform.position = Vector2.MoveTowards(transform.position, henPos, speed * Time.deltaTime);
+		if (Vector2.Distance(transform.position, targetObj.position) > stoppingDistance && following && content)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, henPos, speed * Time.deltaTime);
 			isMoving = true;
 		}
-
-		else
-		{
-			isMoving = false;
-		}
-
-	}
+        if (Vector2.Distance(transform.position, targetObj.position) > letWalk && following && content)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
 
 
     void Flip()
 	{
-		if (hen.position.x > transform.position.x)
-		{
-			mySpriteRenderer.flipX = true;
-		}
-		else if (hen.position.x < transform.position.x)
+		if (targetObj.position.x > transform.position.x)
 		{
 			mySpriteRenderer.flipX = false;
+		}
+
+		else if (targetObj.position.x < transform.position.x)
+		{
+			mySpriteRenderer.flipX = true;
 		}
 	}
 
@@ -72,13 +94,25 @@ public class Chick : MonoBehaviour
 	{
 		hide = GameManager.instance.hideTheChick;
 
+		print("Is moving: " + isMoving);
+		print("Hiding: " + hide);
+
 		if (!isMoving && hide)
 		{
-			mySpriteRenderer.enabled = false;
+			anim.SetBool("Hiding", true);
+            content = true;
+			//mySpriteRenderer.enabled = false;
 		}
+
 		else if (isMoving || !hide)
 		{
-			mySpriteRenderer.enabled = true;
+			anim.SetBool("Hiding", false);
 		}
+	}
+
+   
+    void SetAnimBools()
+	{
+		anim.SetBool("isMoving", isMoving);
 	}
 }
